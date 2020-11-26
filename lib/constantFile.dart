dc26016097df1e329af2235f3684a3daf67494b3
fatima796 <fatimasohail796@gmail.com>
2020-11-26 17:15:02 +0500

    bottom container height 75

--- lib/constantFile.dart
+++ lib/constantFile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const kactiveColor = Color(0xFF1D1E33);
 const kinactiveColor = Color(0xFF111320);
-const kBottomContainerHeight = 80.0;
+const kBottomContainerHeight = 75.0;
 const kBottomContainerColour = Colors.amber;
 
 const kbottemContainerStyle = TextStyle(
