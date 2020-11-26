e02787812dc0e2fe02fdb996562f863a97eb0c3b
fatima796 <fatimasohail796@gmail.com>
2020-11-26 17:52:24 +0500

    ACTIVE color green

--- lib/constantFile.dart
+++ lib/constantFile.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const kactiveColor = Color(0xFF1D1E33);
+const kactiveColor = Color(0xFF1D1E32);
 const kinactiveColor = Color(0xFF111320);
 const kBottomContainerHeight = 75.0;
 const kBottomContainerColour = Colors.amber;
